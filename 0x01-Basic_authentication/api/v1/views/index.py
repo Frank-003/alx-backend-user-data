deec31fbb71f7f4110924939a41dacd66e67e768
Frank-003 <oyiengofrankline49@gmail.com>
2024-09-03 09:49:23 +0300

    task 0

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-from flask import Blueprint, abort
+""" Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
 
-app_views = Blueprint('app_views', __name__)
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
-def unauthorized():
-    abort(401)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
 
