becc0de9ab3096c717d9281b1469a7d35f657bfe
Frank-003 <oyiengofrankline49@gmail.com>
2024-09-03 09:44:57 +0300

    task 1

---
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from flask import Blueprint, abort
+
+app_views = Blueprint('app_views', __name__)
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized():
+    abort(401)
+
