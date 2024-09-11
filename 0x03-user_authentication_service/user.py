9680bb8eda8a540d71717810a2f4189ad6a0a7ad
Frank-003 <oyiengofrankline49@gmail.com>
2024-09-11 09:19:29 +0000

    task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,8 +9,11 @@ class User(Base):
      
     __tablename__ = 'users'
     
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=True)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email})>"
